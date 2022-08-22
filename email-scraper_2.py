51d115fd866f2f8cc097258530487e2e04286d6c
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-08-22 16:12:23 -0400

    Added imports

--- email-scraper_2.py
+++ email-scraper_2.py
@@ -1,1 +1,8 @@
+import imp
+from operator import imod
+from bs4 import BeautifulSoup
 import requests
+import requests.exceptions
+from urllib.parse import urlsplit
+from collections import deque
+import re
