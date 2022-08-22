0799cd7dcd587212649bf48c0efed3fc0533efc6
Mujtaba Zaidi <mujtabazaidi2003@gmail.com>
2022-08-22 16:28:35 -0400

    Getting user input

--- email-scraper_2.py
+++ email-scraper_2.py
@@ -6,3 +6,10 @@ import requests.exceptions
 from urllib.parse import urlsplit
 from collections import deque
 import re
+
+urls = set()
+# Getting the needed urls from the user
+runs = int(input("Enter the number of websites you want the emails from: "))
+for i in range(runs):
+    urls.add(input("Enter the url from which you want emails: "))
+
