e0592991ea941641c4f0af43c1cd37e6cc252b38
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-08-22 16:45:04 -0400

    Dissecting url.

--- email-scraper_2.py
+++ email-scraper_2.py
@@ -1,3 +1,4 @@
+from email.mime import base
 import imp
 from operator import imod
 from bs4 import BeautifulSoup
@@ -7,9 +8,13 @@ from urllib.parse import urlsplit
 from collections import deque
 import re
 
-urls = set()
+urls = []
 # Getting the needed urls from the user
 runs = int(input("Enter the number of websites you want the emails from: "))
 for i in range(runs):
-    urls.add(input("Enter the url from which you want emails: "))
-
+    urls.append(input("Enter the url from which you want emails: "))
+    
+    parts = urlsplit(urls[i])
+    print(parts)
+    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    print(base_url)
