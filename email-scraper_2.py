62620ef1514b90e0d644d3d992e52b05fa706a6b
Mujtaba Zaidi <mujtabazaidi2003@gmail.com>
2022-08-23 14:16:14 -0400

    Preventing duplication and processing on a time basis

--- email-scraper_2.py
+++ email-scraper_2.py
@@ -11,61 +11,86 @@ import re
 import pandas as pd
 import os
 import sys
+import time
 
 urls = []
 emails = []
+masterurls = []
 
 # Getting the needed urls from the user
 runs = int(input("Enter the number of websites you want the emails from: "))
 
 for j in range(runs):
-    urls.append(input("Enter the url from which you want emails: "))
-
-while len(urls) <= 100:
-
+    masterurls.append(input("Enter the url from which you want emails: "))
+start = 0.0
+diff = 0.0
+for em in masterurls:
+    urls.append(em)
+    start = time.perf_counter()
     for k in urls:
-        parts = urlsplit(k)
-        #print(parts)
-        base_url = "{0.scheme}://{0.netloc}".format(parts)
-        #print(base_url)
-        path = k[:k.rfind('/')+1] if '/' in parts.path else k
-        #print(path)
+        if diff <=10:
+            print(diff)
+            parts = urlsplit(k)
+            #print(parts)
+            base_url = "{0.scheme}://{0.netloc}".format(parts)
+            #print(base_url)
+            path = k[:k.rfind('/')+1] if '/' in parts.path else k
+            #print(path)
 
-        try:
-            response = requests.get(k) 
-        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-            continue
+            try:
+                response = requests.get(k) 
+            except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+                continue
 
-        
-        new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+            
+            new_emails = [re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I)]
 
-        if new_emails in emails:
-            continue
+            for m in new_emails:
+                for n in m:
+                        
+                    if n in emails:
+                        continue
+                    else:
+                        emails.append(n)
+
+            soup = BeautifulSoup(response.text, features="lxml")
+
+            for anchor in soup.find_all("a"):
+
+                link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+                if link.startswith('/'):
+                    link = base_url + link
+                elif not link.startswith('http'):
+                    link = path + link
+            
+                if not link in urls:
+                    urls.append(link) 
+
+            desktop_path = os.path.expanduser('~')+'\\Desktop\\emails.csv'
+            file_exists = os.path.exists(desktop_path)
+            if file_exists:
+                df_read = pd.read_csv(desktop_path)
+            else:
+                df.to_csv(desktop_path)
+            print(emails)
+            for item in df_read.values.tolist():
+                if item in emails:
+                    emails.remove(item)
+                else:
+                    df_append = pd.DataFrame(emails, columns=["Email"])
+                
+
+            try:
+                df_append.to_csv(desktop_path, mode='a', index=False, header=False)
+            except PermissionError:
+                print('Please close the csv file.')
+                break
+
+            print(len(urls))
         else:
-            emails.extend(new_emails)
-
-        soup = BeautifulSoup(response.text, features="lxml")
-
-        for anchor in soup.find_all("a"):
-
-            link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-            if link.startswith('/'):
-                link = base_url + link
-            elif not link.startswith('http'):
-                link = path + link
+            urls.clear()
+            
+            diff = 0.0
+            break
+        diff = time.perf_counter() - start
         
-            if not link in urls:
-                urls.append(link) 
-
-
-        print(emails)
-
-        df = pd.DataFrame(emails, columns=["Email"])
-        desktop_path = os.path.expanduser('~')+'\\Desktop\\emails.csv'
-
-        try:
-            df.to_csv(desktop_path, mode='a', index=False, header=False)
-        except PermissionError:
-            print('Please close the csv file.')
-
-        print(len(urls))
